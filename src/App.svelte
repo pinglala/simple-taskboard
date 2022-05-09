f76aad5cf04e08221c421e9e78e79ab84e33fc43
Ping <manping.phu@gmail.com>
2022-05-09 20:41:19 +0200

    #1 Create project

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<main>
+	<h1>Simple Taskboard</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
